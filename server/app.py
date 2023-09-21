fadcdd4f259bbd534fad9af2a9cb36e1d7fb0b46
Brian Ashman <brian.ashman@comcast.net>
2023-09-20 21:05:30 -0700

    Complete Authorization Lab

--- server/app.py
+++ server/app.py
@@ -84,15 +84,22 @@ class CheckSession(Resource):
         
         return {}, 401
 
-class MemberOnlyIndex(Resource):
+@app.before_request
+def check_if_member():
+    if not session.get('user_id') and request.endpoint in ['member_index', 'member_article']:
+        return {'error': 'Unauthorized'}, 401
     
+class MemberOnlyIndex(Resource):   
     def get(self):
-        pass
+        articles = [article.to_dict() for article in Article.query.filter(Article.is_member_only == True).all()]
+        return articles, 200
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        article = Article.query.filter(Article.id == id).first()
+        if article.is_member_only == True:
+            return article.to_dict(), 200
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
